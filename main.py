d01a893ee73629374644f15a406840976d84ede0
Rafael Pereira <71345304+Guaricaya@users.noreply.github.com>
2023-01-09 09:47:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from selenium import webdriver
+rom selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import *
 from time import sleep as s
-import os
-os.system("pip install chromedriver-binary")
-
 # -----------------------Configurações_do_bot------------------------#
 # caminho para executar navegadores baseados no Google Chrome, para funionar o path deve ser atualizado na varaiavel brave_path.
 # brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
 option = Options()
+option.headless = False  # Opera em segundo plano o valor defualt opera em primeiro plano
 option.add_argument("--no-sandbox")
 option.add_argument("--disable-dev-shm-usage")
-#option.headless = True  # Opera em segundo plano o valor defualt opera em primeiro plano
 # Caso queira entrar normalmente pelo chrome basta comentar essa linha
 # option.binary_location = brave_path
 driver = webdriver.Chrome(options=option)
@@ -23,11 +20,12 @@ driver = webdriver.Chrome(options=option)
 
 url = "https://www.google.com/"
 driver.get(url)
+s(10)
 # Achando um elemento com o find_element
 element = driver.find_element(By.NAME, "q")
 element.click()  # clickando no elemento
 element.send_keys("webdriver" + Keys.ENTER)  # digitando com o send_keys.
-# Clica procurando um Classe
+"""# Clica procurando um Classe
 driver.find_element(By.CLASS_NAME, 'gb_d').click()
 print(driver.find_element(By.PARTIAL_LINK_TEXT, 'Fazer').text)
 # Clica procurando um texto entre links
@@ -35,19 +33,15 @@ driver.find_element(By.PARTIAL_LINK_TEXT, 'Fazer').click()
 email = "rafaelbarros"
 s(1)
 driver.find_element(By.NAME, "identifier").send_keys(email)
-s(2)
+s(2)"""
 driver.back()  # Volta para a pagina anterior
 x = [] #Lista para armazenar os elementos 
 item = 2 #Indice de busca
-try:
-    while True:
-        x.append(driver.find_element(
-            By.XPATH, f'//*[@id="hdtb-msb"]//div[{item}]/a').text)
-        item += 1
-except NoSuchElementException:
-    print("Acabou o varredura")
-     
 
+"""while True:
+  x.append(driver.find_element(
+    By.XPATH, f'//*[@id="hdtb-msb"]//div[{item}]/a').text)
+  item += 1"""
 print(x)
 
 
